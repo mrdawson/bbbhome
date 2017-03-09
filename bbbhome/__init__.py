37af1da210e2b8b554a673d5590193717237d086
root <root@arm.localdomain>
2017-03-09 05:06:32 +0000

    GPIO pins work now. need to find workaround for ADC

--- bbbhome/__init__.py
+++ bbbhome/__init__.py
@@ -1,14 +1,18 @@
 from flask import Flask, render_template, jsonify, request
 from flask_basicauth import BasicAuth
-# import Adafruit_BBIO.GPIO as GPIO
-# import Adafruit_BBIO.ADC as ADC
+import Adafruit_BBIO.GPIO as GPIO
+import Adafruit_BBIO.ADC as ADC
+import time
+from apscheduler.schedulers.background import BackgroundScheduler
+
 
 app = Flask(__name__)
 
-file = open("auth.txt", "r")
+scheduler = BackgroundScheduler()
+
+file = open("/var/www/bbbhome/auth.txt", "r")
 auth = file.readlines()
 auth = [x.strip() for x in auth]
-# print (file.readline(), file.readline())
 app.config['BASIC_AUTH_USERNAME'] = auth[0]
 app.config['BASIC_AUTH_PASSWORD'] = auth[1]
 
@@ -16,10 +20,14 @@ basic_auth = BasicAuth(app)
 
 # Initialize GPIO pin
 GPIO.setup("P9_11", GPIO.OUT)
+GPIO.setup("P9_11", GPIO.OUT, delay=50)
 GPIO.setup("P9_13", GPIO.IN)
+GPIO.setup("P9_13", GPIO.IN, delay=50)
 
 # Initialize ADC pin
-ADC.setup()
+#ADC.setup()
+#time.sleep(0.1)
+#ADC.setup()
 analog_pin = "P9_33"
 
 
@@ -48,8 +56,9 @@ def on_off():
 
 @app.route('/get_status')
 def get_status():
-    temp = ADC.read(analog_pin)*1.8
-    temp = temp * 1.8 + 32
+    #temp = ADC.read(analog_pin)*1.8
+    #temp = temp * 1.8 + 32
+    temp = 42
     return jsonify([GPIO.input("P9_13"), temp])
 
 
